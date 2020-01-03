a12375bd2ac42e410c558fbcbaf0557d18277bfa
Ethan Willis <elwillis1@gmail.com>
2020-01-03 16:51:44 -0600

    Added manhattan distance

--- build/index.js
+++ build/index.js
@@ -1,32 +1,33 @@
 "use strict";
 exports.__esModule = true;
 var preconditions = {
-    sameDimensions: function (point1, point2) {
+    sameDimensions: function (vector1, vector2) {
         /**
           TODO: Technically you could find the distance between say
-          a 1-dimensional point and 2-dimensional point by imputing
+          a 1-dimensional vector and 2-dimensional vector by imputing
           the missing dimensions as 0.
           For now, and ease of implementation, we will put this burden
           on the users to fill in the missing dimensions with 0s.
         */
-        if (point1.coordinates.length !== point2.coordinates.length) {
-            throw "Both points must have the same dimension. If you are attempting to find the distance between points of different dimensions you must set the missing dimensions on whichever point is missing them to 0.";
+        if (vector1.coordinates.length !== vector2.coordinates.length) {
+            throw "Both vectors must have the same dimension. If you are attempting to find the distance between vectors of different dimensions you must set the missing dimensions on whichever vector is missing them to 0.";
         }
     }
 };
-var manhanttanDistance = function (point1, point2) {
-    preconditions.sameDimensions(point1, point2);
+var manhanttanDistance = function (vector1, vector2) {
+    preconditions.sameDimensions(vector1, vector2);
     var sum = 0;
-    for (var i = 0; i < point1.coordinates.length; i++) {
-        sum += Math.abs(point1.coordinates[i] - point2.coordinates[i]);
+    for (var i = 0; i < vector1.coordinates.length; i++) {
+        sum += Math.abs(vector1.coordinates[i] - vector2.coordinates[i]);
     }
     return sum;
 };
-var euclideanDistance = function (point1, point2) {
-    preconditions.sameDimensions(point1, point2);
+exports.manhanttanDistance = manhanttanDistance;
+var euclideanDistance = function (vector1, vector2) {
+    preconditions.sameDimensions(vector1, vector2);
     var sum = 0;
-    for (var i = 0; i < point1.coordinates.length; i++) {
-        sum += Math.pow((point1.coordinates[i] - point2.coordinates[i]), 2);
+    for (var i = 0; i < vector1.coordinates.length; i++) {
+        sum += Math.pow((vector1.coordinates[i] - vector2.coordinates[i]), 2);
     }
     return Math.pow(sum, 1) / 2;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,54 @@
 /**
-  This interface can represent points in n-Dimensions
+  This interface can represent vectors in n-Dimensions
 
   Examples:
   1-Dimension
-  const OneDimensionalPoint = {coordinates: [1]} // 1 on the number line.
+  const OneDimensionalVector = {coordinates: [1]} // 1 on the number line.
 
   2-Dimensions
   const TwoDimensionalPOint = {coordinates: [2, 3]} // Would be (x, y) = (2, 3) on a Plane.
 
   ... etc.
 */
-interface NDimensionalPoint {
+interface NDimensionalVector {
   coordinates: [number]
 }
 
 const preconditions = {
-  sameDimensions: (point1:NDimensionalPoint, point2:NDimensionalPoint):void => {
+  sameDimensions: (vector1:NDimensionalVector, vector2:NDimensionalVector):void => {
     /**
       TODO: Technically you could find the distance between say
-      a 1-dimensional point and 2-dimensional point by imputing
+      a 1-dimensional vector and 2-dimensional vector by imputing
       the missing dimensions as 0.
       For now, and ease of implementation, we will put this burden
       on the users to fill in the missing dimensions with 0s.
     */
-    if(point1.coordinates.length !== point2.coordinates.length) {
-      throw "Both points must have the same dimension. If you are attempting to find the distance between points of different dimensions you must set the missing dimensions on whichever point is missing them to 0."
+    if(vector1.coordinates.length !== vector2.coordinates.length) {
+      throw "Both vectors must have the same dimension. If you are attempting to find the distance between vectors of different dimensions you must set the missing dimensions on whichever vector is missing them to 0."
     }
   }
 };
 
-const manhanttanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):number => {
-  preconditions.sameDimensions(point1, point2);
+const manhanttanDistance = (vector1:NDimensionalVector, vector2:NDimensionalVector):number => {
+  preconditions.sameDimensions(vector1, vector2);
 
   let sum = 0
-  for(let i = 0; i < point1.coordinates.length; i++) {
-    sum += Math.abs( point1.coordinates[i] - point2.coordinates[i] )
+  for(let i = 0; i < vector1.coordinates.length; i++) {
+    sum += Math.abs( vector1.coordinates[i] - vector2.coordinates[i] )
   }
 
   return sum;
 };
 
-const euclideanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):number => {
-  preconditions.sameDimensions(point1, point2);
+const euclideanDistance = (vector1:NDimensionalVector, vector2:NDimensionalVector):number => {
+  preconditions.sameDimensions(vector1, vector2);
 
   let sum = 0
-  for(let i = 0; i < point1.coordinates.length; i++) {
-    sum += (point1.coordinates[i] - point2.coordinates[i]) ** 2
+  for(let i = 0; i < vector1.coordinates.length; i++) {
+    sum += (vector1.coordinates[i] - vector2.coordinates[i]) ** 2
   }
 
   return sum ** 1/2
 };
 
-export { euclideanDistance }
+export { euclideanDistance, manhanttanDistance }
