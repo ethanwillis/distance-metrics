99159c211f545662a591a6245a9e76d5155c8874
Ethan Willis <elwillis1@gmail.com>
2020-01-02 20:51:37 -0600

    - Implemented manhattan distance - Refactored preconditions checks into it's own block for re-use

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,34 @@ interface NDimensionalPoint {
   coordinates: [number]
 }
 
-const euclideanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):number => {
-  // Every pair of points must have the same dimensions.
-  /**
-    TODO: Technically you could find the distance between say
-    a 1-dimensional point and 2-dimensional point by imputing
-    the missing dimensions as 0.
-    For now, and ease of implementation, we will put this burden
-    on the users to fill in the missing dimensions with 0s.
-  */
-  if(point1.coordinates.length !== point2.coordinates.length) {
-    throw "Both points must have the same dimension. If you are attempting to find the distance between points of different dimensions you must set the missing dimensions on whichever point is missing them to 0."
+const preconditions = {
+  sameDimensions: (point1:NDimensionalPoint, point2:NDimensionalPoint):void => {
+    /**
+      TODO: Technically you could find the distance between say
+      a 1-dimensional point and 2-dimensional point by imputing
+      the missing dimensions as 0.
+      For now, and ease of implementation, we will put this burden
+      on the users to fill in the missing dimensions with 0s.
+    */
+    if(point1.coordinates.length !== point2.coordinates.length) {
+      throw "Both points must have the same dimension. If you are attempting to find the distance between points of different dimensions you must set the missing dimensions on whichever point is missing them to 0."
+    }
   }
+};
+
+const manhanttanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):number => {
+  preconditions.sameDimensions(point1, point2);
+
+  let sum = 0
+  for(let i = 0; i < point1.coordinates.length; i++) {
+    sum += Math.abs( point1.coordinates[i] - point2.coordinates[i] )
+  }
+
+  return sum;
+};
+
+const euclideanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):number => {
+  preconditions.sameDimensions(point1, point2);
 
   let sum = 0
   for(let i = 0; i < point1.coordinates.length; i++) {
@@ -33,6 +49,6 @@ const euclideanDistance = (point1:NDimensionalPoint, point2:NDimensionalPoint):n
   }
 
   return sum ** 1/2
-}
+};
 
 export { euclideanDistance }
